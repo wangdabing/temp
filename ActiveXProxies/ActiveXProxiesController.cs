0f0dfb4e2349a7fd3161fd3d77a46890fcb9cbb9
wangdabing <wangdabing_nj@qq.com>
2016-02-19 18:41:27 +0800

    22

---
+++ ActiveXProxies/ActiveXProxiesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    /// <summary>
+    /// 插件Js代理
+    /// </summary>
+    public class ActiveXProxiesController : Controller
+    {
+        /***
+
+//插件方法返回值
+{
+  "result": null,
+  "success": false,
+  "error": {
+    "message": "An internal error occured during your request!",
+    "details": "..."
+  },
+  "unAuthorizedRequest": false
+}
+var obj = document.getElementById("xx");
+icp.activeX.player(obj).init().start();
+         */
+
+        /// <summary>
+        /// 获取插件Js代理
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public JavaScriptResult Get(string name)
+        {
+            var js = RenderViewToString("Template");
+            return JavaScript(js);
+        }
+
+        /// <summary>
+        /// 插件Js模板
+        /// </summary>
+        /// <returns></returns>
+        private ActionResult Template()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 将视图转换为字符串
+        /// </summary>
+        /// <param name="viewName"></param>
+        /// <returns></returns>
+        private string RenderViewToString(string viewName)
+        {
+            var view = ViewEngines.Engines.FindView(this.ControllerContext, viewName, null).View;
+            using (var writer = new StringWriter())
+            {
+                var viewContext = new ViewContext(this.ControllerContext, view, this.ViewData, this.TempData, writer);
+                viewContext.View.Render(viewContext, writer);
+                return writer.ToString();
+            }
+        }
+    }
+}
---
+++ ActiveXProxies/PlatPlayer.js
@@ -0,0 +1,89 @@
+﻿/**
+ * 平台播放器接口
+ */
+
+
+
+var icp = icp || {};
+icp.activeX = icp.activeX || {};
+
+(function () {
+    icp.activeX.platPlayer = function (obj) {
+        return new _x(obj);
+    }
+
+    var _x = function (obj) {
+        // ActiveX
+        this.obj = obj;
+
+        // Invoke result
+        this.success = true;
+        this.result = null;
+
+        // Invoke ActiveX
+        this.invoke = function (func, callback) {
+            if (!this.success) {
+                console.info("last step return false, not invoke.")
+                return this;
+            }
+
+            try {
+                this.success = false;
+                this.result = null;
+
+                // Invoke activeX methord
+                var result = func.call(this);
+
+                // Invoke callback
+                if (callback) {
+                    try {
+                        if (callback.call(this, result) === false) {
+                            console.info("callback return false.");
+                            return this;
+                        }
+                    } catch (e) {
+                        console.info("callback error: " + e.message);
+                        return this;
+                    }
+                }
+
+                this.result = result;
+                this.success = true;
+            } catch (e) {
+                console.info("invoke acitveX error: " + e.message);
+            }
+
+            return this;
+        }
+    };
+
+    _x.prototype.constructor = _x;
+
+    _x.prototype.Login = function (ip, port, user, password, avPath, icpCameraId, winobject, callback) {
+        console.info("Login...");
+        return this.invoke(function () {
+            return this.obj.Login(ip, port, user, password, avPath, icpCameraId, winobject);
+        }, callback);
+    };
+
+    _x.prototype.StartPlay = function (callback) {
+        console.info("StartPlay...");
+        return this.invoke(function () {
+            return this.obj.StartPlay();
+        }, callback);
+    };
+
+    _x.prototype.StartBack = function (beginTime, endTime, callback) {
+        console.info("StartBack...");
+        return this.invoke(function () {
+            return this.obj.StartBack(beginTime, endTime);
+        }, callback);
+    };
+
+    _x.prototype.StartProcess = function (cameraId, avPath, startTime, endTime, callback) {
+        console.info("StartProcess...");
+        return this.invoke(function () {
+            return this.obj.StartProcess(beginTime, cameraId, avPath, startTime, endTime);
+        }, callback);
+    };
+})();
